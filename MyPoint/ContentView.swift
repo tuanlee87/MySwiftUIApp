795fda9129355f7b96dd33fc4ebd36411c33c9a1
Lee Tún <tuanlee101.dev@gmail.com>
2025-08-01 00:22:47 +0700

    Initial Commit

---
+++ MyPoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPoint
+//
+//  Created by Lee Tún on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPoint/MyPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPointApp.swift
+//  MyPoint
+//
+//  Created by Lee Tún on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
